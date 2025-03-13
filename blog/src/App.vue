cd83d7a0b41fc78906c4a7dba0e0ff74780745d2
TylerDurden-cmd <125083855+TylerDurden-cmd@users.noreply.github.com>
2025-03-13 23:04:12 +0100

    navigation component in vue main website logo

---
+++ blog/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import navigationComp from './components/navigation.vue'
+export default {
+  name: 'App',
+  components: {
+    navigationComp
+  }
+}
+</script>
+
+
+<template>
+  <div>
+    <navigationComp></navigationComp>
+    <router-view></router-view>
+  </div>
+</template>
