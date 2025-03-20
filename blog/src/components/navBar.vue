75a7ccaf0474452378e62b53b5d91c199e974d6a
TylerDurden-cmd <125083855+TylerDurden-cmd@users.noreply.github.com>
2025-03-20 14:12:08 +0100

    improved navigation and deleted sing ing

--- blog/src/components/navBar.vue
+++ blog/src/components/navBar.vue
@@ -1,35 +1,58 @@
 <template>
-    <nav class="bg-gray-800 text-white p-4 flex justify-between items-center">
-        <div>
-            <img src="../assets/logoipsum-288.svg" class="h-7" alt="Logo Ipsum">
-        </div>
-        <ul class="flex justify-end space-x-6 mr-10">
-            <li>
-                <router-link to="/">Home</router-link>
-            </li>
-            <li>
-                <router-link to="/about">About</router-link>
-            </li>
-            <li>
-                <router-link to="/contact">Contact</router-link>
-            </li>
-            <li>
-                <router-link to="/signin">Sign In</router-link>
-            </li>
-            <li>
-                <router-link to="/register">Register</router-link>  
-            </li>
-            <li>
-                <router-link to="/blogeditor">Blog Editor</router-link>
-            </li>
-            <li>
-                <button @click="handleSignOut" v-if="isLoggedIn">Sign out</button>
-            </li>
-            <li>
-                <router-link to="/blog">Blog</router-link>
-            </li>
-        </ul>
-    </nav>
+<nav class="bg-gray-800 text-white p-4 flex justify-between items-center">
+    <!-- Logo -->
+    <div>
+      <img src="../assets/logoipsum-288.svg" class="h-7" alt="Logo Ipsum">
+    </div>
+
+    <!-- Navigation Links -->
+    <ul class="hidden md:flex justify-end space-x-6 mr-10">
+      <li><router-link to="/" class="hover:text-gray-300">Home</router-link></li>
+      <li><router-link to="/blog" class="hover:text-gray-300">Blog</router-link></li>
+      <li v-if="isLoggedIn">
+        <router-link to="/blogeditor" class="hover:text-gray-300">Blog Editor</router-link>
+      </li>
+      <li><router-link to="/about" class="hover:text-gray-300">About</router-link></li>
+      <li><router-link to="/contact" class="hover:text-gray-300">Contact</router-link></li>
+
+      <template v-if="!isLoggedIn">
+        <li><router-link to="/register" class="hover:text-gray-300">Register</router-link></li>
+      </template>
+
+      <li v-if="isLoggedIn">
+        <button @click="handleSignOut" class="text-red-400 hover:text-red-600">Sign out</button>
+      </li>
+    </ul>
+
+    <!-- Mobile Menu Button -->
+    <button @click="toggleMenu" class="md:hidden">
+      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M4 6h16M4 12h16m-7 6h7"></path>
+      </svg>
+    </button>
+
+    <!-- Mobile Menu -->
+    <div v-if="isMenuOpen" class="absolute top-14 right-4 bg-gray-900 p-4 rounded shadow-lg md:hidden">
+      <ul class="flex flex-col space-y-4">
+        <li><router-link to="/" @click="toggleMenu">Home</router-link></li>
+        <li><router-link to="/blog" @click="toggleMenu">Blog</router-link></li>
+        <li v-if="isLoggedIn">
+          <router-link to="/blogeditor" @click="toggleMenu">Blog Editor</router-link>
+        </li>
+        <li><router-link to="/about" @click="toggleMenu">About</router-link></li>
+        <li><router-link to="/contact" @click="toggleMenu">Contact</router-link></li>
+
+        <template v-if="!isLoggedIn">
+          <li><router-link to="/register" @click="toggleMenu">Register</router-link></li>
+        </template>
+
+        <li v-if="isLoggedIn">
+          <button @click="handleSignOut" class="text-red-400 hover:text-red-600">Sign out</button>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script setup>
